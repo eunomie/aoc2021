4e4ef16a9a37232ece55d1079c8959271cf4d047
Yves Brissaud <yves.brissaud@gmail.com>
2021-12-02 20:55:31 +0100

    day 2 - part 2

--- day2/src/main.rs
+++ day2/src/main.rs
@@ -3,6 +3,7 @@ use std::fs;
 fn main() {
     let input = fs::read_to_string("input.txt").expect("could not read input file");
     println!("part 1: {}", part1(&input));
+    println!("part 2: {}", part2(&input));
 }
 
 fn part1(input: &str) -> i32 {
@@ -24,6 +25,27 @@ fn part1(input: &str) -> i32 {
     return horiz * depth;
 }
 
+fn part2(input: &str) -> i32 {
+    let mut aim: i32 = 0;
+    let mut horiz: i32 = 0;
+    let mut depth: i32 = 0;
+
+    input.lines().for_each(|line| {
+        let tokens: Vec<&str> = line.splitn(2, ' ').collect();
+        let order = tokens[0];
+        let v: i32 = tokens[1].trim().parse().expect("int expected");
+        if order == "forward" {
+            horiz += v;
+            depth += aim * v;
+        } else if order == "down" {
+            aim += v;
+        } else if order == "up" {
+            aim -= v;
+        }
+    });
+    return horiz * depth;
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -38,5 +60,17 @@ down 8
 forward 2";
 
         assert_eq!(part1(input), 150);
+    }
+
+    #[test]
+    fn test_part2() {
+        let input = "forward 5
+down 5
+forward 8
+up 3
+down 8
+forward 2";
+
+        assert_eq!(part2(input), 900);
     }
 }
